380250b922dca5dcbe35afcdf9fe6f9f11b595ae
Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>
2016-11-28 18:18:54 +0100

    Fix linker file
    Signed-off-by: Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
@@ -15,8 +15,41 @@
 #include <bsp.h>
 #include <bsp/start.h>
 
+inline void debugPrint(void)
+{
+	register uint32_t num_res asm("r0") = 8;
+	register uint32_t arg1 asm("r1") = '\0';
+
+	char *filename = __FILE__;
+  char *line;
+  char tmp;
+
+	for (char *ptr = filename; *ptr++; )
+	{
+    arg1 = *ptr;
+    asm volatile(
+      ".arch_extension virt\n\t"
+      "hvc #0x4a48\n\t"
+      : "=r" (num_res)
+      : "r" (num_res), "r" (arg1)
+      : "memory");
+  }
+  line = itoa(__LINE__);
+	for (char *ptr = line; *ptr++; )
+	{
+    arg1 = *ptr;
+    asm volatile(
+      ".arch_extension virt\n\t"
+      "hvc #0x4a48\n\t"
+      : "=r" (num_res)
+      : "r" (num_res), "r" (arg1)
+      : "memory");
+  }
+}
+
 void BSP_START_TEXT_SECTION bsp_start_hook_0(void)
 {
+  debugPrint();
 }
 
 void BSP_START_TEXT_SECTION bsp_start_hook_1(void)
