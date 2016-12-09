4ec18bea73a837e7212e46bdf5a29eb238df2a16
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-09 10:34:49 +0100

    Add Jetson-TK1 board support
    Add Tegra-specific make config:
      - armv7-a architecture
      - vector floating point unit
      - Tegra3 is based on arm cortex-a15 processor
    
    Add console:
      - Use UART-A and UART-D
      - Choose in
    
    Jailhouse:
      - Define in jetson-tk1.h JAILHOUSE_ENABLE (needed atm)
      - Add jailhouse debug console
        - Satisfies pre-console in/out using bspIo
      - Add own gic handler (jailhouse forbids inter-cell GICD access)
    
    Add Timercounter support
      - Use the RTEMS timercounter API using the ARM Generic Timer.
    
    Add Timer support
      - Benchmark RTEMS using the Nvidia TMRUS

---
+++ c/src/lib/libbsp/arm/jetson-tk1/include/jailhouse.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) OTH Regensburg, 2017
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef LIBBSP_ARM_JETSONTK1_JAILHOUSE_H
+#define LIBBSP_ARM_JETSONTK1_JAILHOUSE_H
+
+#include <bspopts.h>
+#include <stdint.h>
+#include <bsp/jetson-gpio.h>
+#include <bsp/console.h>
+#include <rtems/termiostypes.h>
+
+#define PRIMARY_CPU       0x8
+
+static uint8_t jailhouse_cpu_list[1] = {
+  PRIMARY_CPU,
+};
+static uint32_t jailhouse_irq_list[1] = {
+#if NS8250_CONSOLE_USE_INTERRUPTS == 1
+  UARTD_IRQ,
+#if NS8250_USE_SECONDARY_CONSOLE == 1
+  UARTA_IRQ,
+#endif
+#endif
+  GPIO6_IRQ,
+};
+
+#endif /* LIBBSP_ARM_JETSONTK1_JAILHOUSE_H */
