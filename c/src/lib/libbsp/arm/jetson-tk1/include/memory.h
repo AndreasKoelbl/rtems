71dc61adeee77bf5b5d9a9b7de4543640eac635e
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-09 10:34:49 +0100

    Add jetson-tk1 support
    Add tegra-specific make config:
      - armv7-a architecture
      - vector floating point unit
      - tegra3 is based on arm cortex-a15 processor
    
    Add console:
      - Use UART-A and UART-D
      - Add debug console
    
    Jailhouse:
      - Define in bsp.h JAILHOUSE_ENABLE
      - Add jailhouse debug console
      - Modify gic handler (jailhouse forbids inter-cell GICD access)
    
    Add Timercounter support
      - Use the RTEMS timercounter API using the ARM Generic Timer.
    
    Add Benchmark Timer support
      - Benchmark RTEMS using the Nvidia TMRUS

---
+++ c/src/lib/libbsp/arm/jetson-tk1/include/memory.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) OTH Regensburg, 2016
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef LIBBSP_ARM_JETSONTK1_MEMORY_H
+#define LIBBSP_ARM_JETSONTK1_MEMORY_H
+
+#define SYSREG_32(...) 32, __VA_ARGS__
+#define SYSREG_64(...) 64, __VA_ARGS__
+
+#define _arm_read_sysreg(size, ...) arm_read_sysreg_ ## size(__VA_ARGS__)
+#define arm_read_sysreg(...) _arm_read_sysreg(__VA_ARGS__)
+asm(".arch_extension virt\n");
+
+#define arm_write_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mcr	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: : "r"((uint32_t)(val)))
+#define arm_write_sysreg_64(op1, crm, val) \
+	asm volatile ("mcrr	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: : "r"((uint64_t)(val)))
+
+#define arm_read_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mrc	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: "=r"((uint32_t)(val)))
+#define arm_read_sysreg_64(op1, crm, val) \
+	asm volatile ("mrrc	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: "=r"((uint64_t)(val)))
+
+static inline uint32_t mmio_read32(void *address)
+{
+	return *(volatile uint32_t *)address;
+}
+
+static inline void mmio_write32(void *address, uint32_t value)
+{
+	*(volatile uint32_t *)address = value;
+}
+
+static inline unsigned char mmio_read8(void *address)
+{
+	return *(volatile unsigned char *)address;
+}
+
+static inline void mmio_write8(void *address, unsigned char value)
+{
+	*(volatile unsigned char *)address = value;
+}
+
+static inline void mmio_write16(void *address, unsigned short value)
+{
+	*(volatile unsigned short *)address = value;
+}
+
+#endif /* LIBBSP_ARM_JETSONTK1_MEMORY_H */
--- c/src/lib/libbsp/arm/shared/include/arm-gic.h
+++ c/src/lib/libbsp/arm/shared/include/arm-gic.h
@@ -24,6 +24,9 @@
 #define LIBBSP_ARM_SHARED_ARM_GIC_H
 
 #include <bsp/arm-gic-regs.h>
+#ifdef JAILHOUSE_ENABLE
+#include <bsp/jailhouse.h>
+#endif
 
 #include <stdbool.h>
 
@@ -121,6 +124,27 @@ static inline void gic_id_set_targets(
   uint8_t targets
 )
 {
+#ifdef JAILHOUSE_ENABLE
+  unsigned int i;
+  bool success = false;
+  for (i = 0; i < RTEMS_ARRAY_SIZE(jailhouse_cpu_list); i++) {
+    if (jailhouse_cpu_list[i] == targets) {
+      success = true;
+      break;
+    }
+  }
+  if (!success)
+    return;
+  success = false;
+  for (i = 0; i < RTEMS_ARRAY_SIZE(jailhouse_irq_list); i++) {
+    if (jailhouse_irq_list[i] == id) {
+      success = true;
+      break;
+    }
+  }
+  if (!success)
+    return;
+#endif
   dist->icdiptr[id] = targets;
 }
 
