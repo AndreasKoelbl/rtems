fa1830262c483f9bdeb91bf8bd9d16d0b02b67f4
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-16 19:28:38 +0100

    Mockup functions needed by RTEMS
    RTEMS uses a basic set of functions which have to be implemented.
    This affects:
      - irq
      - smp
      - clock
      - bspreset
      - bspgetworkarea
    
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/misc/timer.c
+++ c/src/lib/libbsp/arm/jetson-tk1/misc/timer.c
@@ -27,18 +27,11 @@ static uint64_t benchmark_timer_base;
 
 void benchmark_timer_initialize( void )
 {
-  benchmark_timer_base = BCM2835_REG( BCM2835_GPU_TIMER_CLO );
 }
 
 benchmark_timer_t benchmark_timer_read( void )
 {
-  uint32_t delta = BCM2835_REG( BCM2835_GPU_TIMER_CLO ) - benchmark_timer_base;
-
-  if ( benchmark_timer_find_average_overhead ) {
-    return delta;
-  } else {
-    return BCM2835_REG( BCM2835_GPU_TIMER_CLO );
-  }
+  return 0;
 }
 
 void benchmark_timer_disable_subtracting_average_overhead(
