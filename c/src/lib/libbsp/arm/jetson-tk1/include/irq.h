591baffa5c1c80607af3b4d53010cddfd19797e3
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-16 21:40:08 +0100

    Part of filling irq
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/include/irq.h
+++ c/src/lib/libbsp/arm/jetson-tk1/include/irq.h
@@ -23,19 +23,26 @@
 #include <rtems/irq.h>
 #include <rtems/irq-extension.h>
 
+/* architecture specific (tegra)*/
 #define TIMER_IRQ                   27
+
+#define GICD_V2_BASE	((void *)0x50041000)
+#define GICC_V2_BASE	((void *)0x50042000)
+
+/* GICv2 specific */
+#define GICC_CTLR		0x0000
+#define GICC_PMR		0x0004
+#define GICC_IAR		0x000c
+#define GICC_EOIR		0x0010
+
+#define GICC_CTLR_GRPEN1	(1 << 0)
+
+#define GICC_PMR_DEFAULT	0xf0
+
 
 #define BSP_INTERRUPT_VECTOR_MIN    0
 /* TODO: Fix random number */
 #define BSP_INTERRUPT_VECTOR_MAX    64
-
-/**
- * @defgroup raspberrypi_interrupt Interrrupt Support
- *
- * @ingroup jetsontk1_interrupt
- *
- * @brief Interrupt support.
- */
 
 #endif /* ASM */
 #endif /* LIBBSP_ARM_JETSONTK1_IRQ_H */
