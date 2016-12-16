47eae21f1ae511ea633bd5dafe6a1881a0a00810
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-16 19:28:38 +0100

    Mockup functions needed by RTEMS
    RTEMS uses a basic set of functions which have to be implemented.
    This affects:
      - irq
      - smp
      - clock
      - bspreset
      - bspgetworkarea
    
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspreset.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspreset.c
@@ -1,26 +1,10 @@
-/**
- * @file
- *
- * @ingroup raspberrypi
- *
- * @brief Raspberry pi restart chip function
- */
-
 #include <bsp/bootcard.h>
-#include <bsp/raspberrypi.h>
 #include <bsp.h>
 #include <rtems.h>
 
 void bsp_reset(void)
 {
-  uint32_t rstc;
-
-  BCM2835_REG(BCM2835_PM_WDOG) = BCM2835_PM_PASSWD_MAGIC | 20;
-  rstc = BCM2835_REG(BCM2835_PM_RSTC);
-  rstc &= ~BCM2835_PM_RSTC_WRCFG;
-  rstc |= BCM2835_PM_PASSWD_MAGIC | BCM2835_PM_RSTC_WRCFG_FULL;
-  BCM2835_REG(BCM2835_PM_RSTC) = rstc;
-  BCM2835_REG(BCM2835_PM_WDOG) = BCM2835_PM_PASSWD_MAGIC | 1;
+  printk("Jailhouse is not capable of resetting - wait\n");
 
   while (1) ;
 }
