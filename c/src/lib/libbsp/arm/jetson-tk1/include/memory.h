fd2529c39c80733573417577064b6a264890bbd6
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2016-12-27 13:39:42 +0100

    Add memory accessing functions
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/include/memory.h
+++ c/src/lib/libbsp/arm/jetson-tk1/include/memory.h
@@ -17,6 +17,27 @@
 #ifndef LIBBSP_ARM_JETSONTK1_MEMORY_H
 #define LIBBSP_ARM_JETSONTK1_MEMORY_H
 
+#define SYSREG_32(...) 32, __VA_ARGS__
+#define SYSREG_64(...) 64, __VA_ARGS__
+
+#define _arm_read_sysreg(size, ...) arm_read_sysreg_ ## size(__VA_ARGS__)
+#define arm_read_sysreg(...) _arm_read_sysreg(__VA_ARGS__)
+asm(".arch_extension virt\n");
+
+#define arm_write_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mcr	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: : "r"((uint32_t)(val)))
+#define arm_write_sysreg_64(op1, crm, val) \
+	asm volatile ("mcrr	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: : "r"((uint64_t)(val)))
+
+#define arm_read_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mrc	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: "=r"((uint32_t)(val)))
+#define arm_read_sysreg_64(op1, crm, val) \
+	asm volatile ("mrrc	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: "=r"((uint64_t)(val)))
+
 static inline uint32_t mmio_read32(void *address)
 {
 	return *(volatile uint32_t *)address;
