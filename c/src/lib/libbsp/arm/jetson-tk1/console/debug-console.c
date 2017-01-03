b60dada9a0a4df9e1ba8b8063262ce6d2285a50c
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-01-03 09:22:27 +0100

    Add jailhouse debug console:
      - Serves jailhouse_debug_console_write(char*) which results in hypercalls
      - Provides print_hex
    
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/console/debug-console.c
+++ c/src/lib/libbsp/arm/jetson-tk1/console/debug-console.c
@@ -1,55 +1,115 @@
 /**
  * @file
  *
- * @ingroup arm_jetson-tk1
+ * @ingroup Jailhouse Hypervisor Debug Console
  *
- * @brief Jetson-TK1 console driver
- *
+ * @brief This helps creating a jailhouse hypervisor guest
  */
 
 /*
- * Copyright (c) OTH Regensburg, 2016
+ *  COPYRIGHT (c) 1989-2011, 2016.
+ *  On-Line Applications Research Corporation (OAR).
  *
- * The license and distribution terms for this file may be
- * found in the file LICENSE in this distribution or at
- * http://www.rtems.org/license/LICENSE.
+ *  The license and distribution terms for this file may be
+ *  found in the file LICENSE in this distribution or at
+ *  http://www.rtems.org/license/LICENSE.
  */
 
-#include <bsp/console.h>
-#include <bsp/jetson-tk1.h>
-#include <rtems/bspIo.h>
 #include <rtems/sysinit.h>
+#include <rtems/bspIo.h>
 
-#define JAILHOUSE_HC_DEBUG_CONSOLE_PUTC 8
-
-static void jetsontk1_debug_console_out(char c)
+void jailhouse_debug_console_out(char c)
 {
-  hypervisor_putc(c);
+  register uint32_t num_res asm("r0") = 8;
+	register uint32_t arg1 asm("r1") = c;
+
+	asm volatile(
+		".arch_extension virt\n\t"
+		"hvc #0x4a48\n\t"
+		: "=r" (num_res)
+		: "r" (num_res), "r" (arg1)
+		: "memory");
 }
 
-static void jetsontk1_debug_console_init(void)
+static void swap(char *first, char *second)
 {
-  BSP_output_char = jetsontk1_debug_console_out;
+  char temp = *first;
+  *first = *second;
+  *second = temp;
 }
 
-static void jetsontk1_debug_console_early_init(char c)
+static void reverse(char str[], int length)
 {
-  jetsontk1_debug_console_init();
-  jetsontk1_debug_console_out(c);
+  int start = 0;
+  int end = length -1;
+  while (start < end)
+  {
+      swap(str + start, str + end);
+      start++;
+      end--;
+  }
 }
 
-static int jetsontk1_debug_console_in(void)
+void myItoa(uint32_t num, char* str, uint32_t base)
 {
-  /* we do not read yet... */
-  return (int)'Y';
+  int i = 0;
+
+  /* Handle 0 explicitely, otherwise empty string is printed for 0 */
+  if (num == 0)
+  {
+    str[i++] = '0';
+    str[i] = '\0';
+    return str;
+  }
+
+  while (num != 0)
+  {
+    int rem = num % base;
+    if (rem > 9)
+    {
+      str[i++] = 'A' + (rem - 10);
+    }
+    else
+    {
+      str[i++] = '0' + rem;
+    }
+    num /= base;
+  }
+
+  str[i] = '\0'; // Append string terminator
+
+  reverse(str, i);
 }
 
-BSP_output_char_function_type BSP_output_char = jetsontk1_debug_console_early_init;
+void jailhouse_debug_console_write(char *text)
+{
+  do
+  {
+    jailhouse_debug_console_out(*text);
+  } while (*text++);
+}
 
-BSP_polling_getchar_function_type BSP_poll_char = jetsontk1_debug_console_in;
+void print_hex(uint32_t num)
+{
+  uint8_t size = 0;
+  char str[9];
+  const char* prefix = "0x";
+
+  myItoa(num, str, 16);
+
+  jailhouse_debug_console_write(prefix);
+  jailhouse_debug_console_write(str);
+}
+
+static void jailhouse_debug_console_init(void)
+{
+  BSP_output_char = jailhouse_debug_console_out;
+}
+
+BSP_output_char_function_type BSP_output_char;
 
 RTEMS_SYSINIT_ITEM(
-  jetsontk1_debug_console_init,
+  jailhouse_debug_console_init,
   RTEMS_SYSINIT_BSP_START,
-  RTEMS_SYSINIT_ORDER_FIRST
+  RTEMS_SYSINIT_ORDER_LAST
 );
