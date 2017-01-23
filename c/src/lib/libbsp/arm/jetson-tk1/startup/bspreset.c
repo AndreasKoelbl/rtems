f5c906b8279742e9bc327b2752f1af7a7dd91702
Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>
2017-01-23 13:26:21 +0100

    bspreset Cleanup
    Signed-off-by: Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspreset.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspreset.c
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) OTH Regensburg, 2016
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
 #include <bsp/bootcard.h>
 #include <bsp.h>
 #include <rtems.h>
