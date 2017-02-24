c31ee312245debd480f893346f997d0f44a93a40
Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>
2017-02-24 15:53:49 +0100

    Move back to jailhouse
    Signed-off-by: Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
@@ -15,7 +15,11 @@
 void BSP_START_TEXT_SECTION bsp_start_hook_0(void)
 {
   arm_cp15_set_translation_table_base_control_register(0x0);
+#ifdef JAILHOUSE_ENABLE
+  arm_cp15_set_vector_base_address((void*) 0x00000000);
+#else
   arm_cp15_set_vector_base_address((void*) 0x90000000);
+#endif
 }
 
 void BSP_START_TEXT_SECTION bsp_start_hook_1(void)
