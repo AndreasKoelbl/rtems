d94a227f6b8265512881b3532497155779d3d879
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-02-28 23:11:16 +0100

    Add basic gpio
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

---
+++ c/src/lib/libbsp/arm/jetson-tk1/include/gpio.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) OTH Regensburg, 2017
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+#ifndef LIBBSP_ARM_JETSONTK1_GPIO_H
+#define LIBBSP_ARM_JETSONTK1_GPIO_H
+
+#define GPIO_BASE ((void*)0x6000d000)
+#define PINMUX_BASE ((void*)0x70000000)
+
+#endif /* LIBBSP_ARM_JETSONTK1_GPIO_H */
