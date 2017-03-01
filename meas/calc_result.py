64b7b8ac36dc8fae7c0e2cd39768836bee86c7aa
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-03-01 08:17:40 +0100

    Fix calc_result.py
    Signed-off-by: Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>

--- meas/calc_result.py
+++ meas/calc_result.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Je länger NUM_TICKS desto länger jitter, skaliert linear
+import sys
 
-from operator import itemgetter
+def main():
+    try:
+      file = ""
+      if len(sys.argv) > 1:
+        file = sys.argv[1]
+      else:
+        file = "result.txt"
 
-lines = [line.rstrip('\n') for line in open('result.txt')]
-secs = []
-nsecs = []
-for i,line in zip(range(len(lines)), lines):
-    result = line.split(':')
-    secs.append(int(result[0].strip()))
-    nsecs.append(int(result[1].strip()))
+      lines = [line.rstrip('\n') for line in open(file)]
+    except IOError as ioe:
+        sys.stderr.write("Caught IOError: {}".format(repr(ioe)))
+        sys.exit(1)
 
-diff = []
-for i in range(len(secs)-1):
-    diff.append((secs[i+1]-secs[i]) * 1000000000 + nsecs[i+1]-nsecs[i])
-    print(diff[i])
-print('min: {0} max: {1} avg: {2}'.format(min(diff), max(diff),
-sum(diff)/len(diff)))
+    secs = []
+    nsecs = []
+    for i,line in zip(range(len(lines)), lines):
+        result = line.split(':')
+        secs.append(int(result[0].strip()))
+        nsecs.append(int(result[1].strip()))
+
+    diff = []
+    for i in range(len(secs)-1):
+        diff.append((secs[i+1]-secs[i]) * 1000000000 + nsecs[i+1]-nsecs[i])
+        print(diff[i])
+    print('min: {0} max: {1} avg: {2}'.format(min(diff), max(diff),
+    sum(diff)/len(diff)))
+
+if __name__ == '__main__':
+    main()
