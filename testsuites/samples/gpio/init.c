c0e75c9ee0d3d7404307d57d7cf9a71272b0e066
Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>
2017-03-02 20:11:06 +0100

    samples: test more functions
    Signed-off-by: Andreas Koelbl <andreas.koelbl@st.oth-regensburg.de>

--- testsuites/samples/gpio/init.c
+++ testsuites/samples/gpio/init.c
@@ -11,11 +11,10 @@
 #include "config.h"
 #endif
 
-#include <rtems/printer.h>
-#include <rtems/test.h>
 #include <rtems/irq-extension.h>
 
 #include <bsp.h> /* for device driver prototypes */
+#include <bsp/memory.h>
 #include <bsp/gpio.h>
 
 #include <stdio.h>
@@ -37,6 +36,9 @@
     perror(#msg); \
   }
 
+#define GPIO_INT_ENB 0x50
+#define GPIO_BASE    0x70003000
+
 rtems_task Init(rtems_task_argument argument);
 void irq_handler(void *arg);
 
@@ -45,19 +47,32 @@ rtems_task Init(rtems_task_argument ignored)
   rtems_status_code status;
   rtems_vector_number vector;
 
-  vector = rtems_gpio_bsp_get_vector(5);
+  vector = rtems_gpio_bsp_get_vector(6);
+  rtems_gpio_bsp_clear(6, 6);
+  rtems_gpio_bsp_disable_interrupt(6, 6, RISING_EDGE);
+
   status = rtems_interrupt_handler_install(
     vector,
     "GPIO6",
     RTEMS_INTERRUPT_UNIQUE,
     irq_handler,
-    (char*) vector/* TODO: transmit context */
+    (char*) vector
   );
-  rtems_directive_failed(status, "interrupt_handler_install");
+  printf("after installing\n");
+  status = rtems_gpio_bsp_select_output(6, 6, NULL);
+  rtems_directive_failed(status, "select output");
+//  mmio_write32(GPIO_BASE + (6 - 1) * 0x100 + GPIO_INT_ENB, 1 << 4);
+  status = rtems_gpio_bsp_select_input(6, 4, NULL);
+  rtems_directive_failed(status, "select input");
+  status = rtems_gpio_bsp_enable_interrupt(6, 4, RISING_EDGE);
+  rtems_directive_failed(status, "enable interrupt");
 
-  status = rtems_gpio_bsp_select_output(5, 0, NULL);
-  status = rtems_gpio_bsp_enable_interrupt(5, 0, vector);
-  status = rtems_gpio_bsp_set(5, 0);
+  status = rtems_gpio_bsp_set(6, 6);
+  rtems_directive_failed(status, "gpio set");
+  rtems_task_wake_after(50);
+  rtems_gpio_bsp_clear(6, 6);
+  printf("entering endless loop\n");
+
   while (true) {
     asm volatile("" ::: "memory");
   }
@@ -65,19 +80,19 @@ rtems_task Init(rtems_task_argument ignored)
 
 void irq_handler(void *arg)
 {
-  printf("irq received\n");
-  rtems_gpio_bsp_interrupt_line((rtems_vector_number) arg);
+  rtems_gpio_bsp_clear(6, 6);
+  printf("irqn: %u\n", (rtems_vector_number) arg);
 }
 
 /* NOTICE: the clock driver is explicitly disabled */
-#define CONFIGURE_APPLICATION_DOES_NOT_NEED_CLOCK_DRIVER
+#define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
 #define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
+
+#define CONFIGURE_MICROSECONDS_PER_TICK 100000
 
 #define CONFIGURE_MAXIMUM_TASKS            1
 
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
-
-#define CONFIGURE_INITIAL_EXTENSIONS RTEMS_TEST_INITIAL_EXTENSION
 
 #define CONFIGURE_INIT
 #include <rtems/confdefs.h>
