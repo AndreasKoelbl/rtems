eb0a8832b270faf81dcab0fb691e11fa0e4294d5
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-05-24 17:04:03 +0200

    samples: hello: implement memtest

--- testsuites/samples/hello/init.c
+++ testsuites/samples/hello/init.c
@@ -16,9 +16,13 @@
 
 #include <bsp.h> /* for device driver prototypes */
 #include <bsp/bootcard.h>
+#include <bsp/cmdline.h>
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PAGE_SIZE 4096
 
 /* forward declarations to avoid warnings */
 rtems_task Init(rtems_task_argument argument);
@@ -26,15 +30,40 @@ rtems_task Init(rtems_task_argument argument);
 const char rtems_test_name[] = "HELLO WORLD";
 rtems_printer rtems_test_printer;
 
+static void *pool[64000];
+
+#define MALLOC_SIZE (PAGE_SIZE / 2)
+
 rtems_task Init(
   rtems_task_argument ignored
 )
 {
+  unsigned int i = 0;
+  void *mem;
+
   rtems_print_printer_printf(&rtems_test_printer);
   rtems_test_begin();
   printf( "Hello World\n" );
   printf( "Pointer to commandlines: %p\n", bsp_boot_cmdline);
   printf( "cmdline: %s\n", bsp_boot_cmdline);
+  printf( "Begin of memtest\n");
+
+  printf("Allocating...\n");
+  for (;;) {
+    mem = malloc(MALLOC_SIZE);
+    if (!mem)
+      break;
+    memset(mem, 0xaa, MALLOC_SIZE);
+    pool[i++] = mem;
+  }
+  printf("Done allocating %u segments\n", i);
+
+  printf("Freeing...\n");
+  for (i = 0; pool[i]; i++) {
+    free(pool[i]);
+  }
+  printf("Done.\n");
+
   rtems_test_end();
   exit( 0 );
 }
