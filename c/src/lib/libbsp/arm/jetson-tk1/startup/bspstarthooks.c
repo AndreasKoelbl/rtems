63453e028f6b51bb02d680b0dab9904640b6d9ed
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-05-26 10:54:42 +0200

    Revert "mmu: Try using default sections"
    This reverts commit 5316b4ffd3a57e17b401eb6e4ee9c5fd7212c675.

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
@@ -11,23 +11,7 @@
 #include <bsp/mm.h>
 #include <bsp.h>
 #include <libcpu/arm-cp15.h>
-#include <bsp/arm-cp15-start.h>
-
-static void BSP_START_TEXT_SECTION setup_mmu_and_cache(void)
-{
-  uint32_t ctrl = arm_cp15_start_setup_mmu_and_cache(
-    ARM_CP15_CTRL_A | ARM_CP15_CTRL_M,
-    ARM_CP15_CTRL_AFE | ARM_CP15_CTRL_Z
-  );
-
-  arm_cp15_start_setup_translation_table_and_enable_mmu_and_cache(
-    ctrl,
-    (uint32_t *) bsp_translation_table_base,
-    ARM_MMU_DEFAULT_CLIENT_DOMAIN,
-    &arm_cp15_start_mmu_config_table[0],
-    arm_cp15_start_mmu_config_table_size
-  );
-}
+#include <bsp/bootcard.h>
 
 void BSP_START_TEXT_SECTION bsp_start_hook_0( void )
 {
@@ -43,22 +27,10 @@ void BSP_START_TEXT_SECTION bsp_start_hook_0( void )
     ctrl &= ~ARM_CP15_CTRL_V;
     arm_cp15_set_control(ctrl);
   }
-  printk("before invalidate\n");
-  arm_cp15_instruction_cache_invalidate();
-  arm_cp15_data_cache_invalidate_all_levels();
-  printk("after invalidate\n");
 }
 
 void BSP_START_TEXT_SECTION bsp_start_hook_1( void )
 {
   bsp_start_copy_sections();
-  printk("before mmu\n");
-  setup_mmu_and_cache();
-  printk("after mmu\n");
-#ifndef RTEMS_SMP
-  /* Enable unified L2 cache */
-  rtems_cache_enable_data();
-#endif
   bsp_start_clear_bss();
-
 }
