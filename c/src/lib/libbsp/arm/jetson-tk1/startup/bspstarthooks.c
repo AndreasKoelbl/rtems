82080cc4888a0fe8c1a60068967b9d05efef72b4
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-06-06 15:26:36 +0200

    get mmu running under JH
    this patch disables the mmu, if active, and enables the mmu afterwards.
    
    Enabling currently only works under jailhouse.

--- c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
+++ c/src/lib/libbsp/arm/jetson-tk1/startup/bspstarthooks.c
@@ -11,26 +11,56 @@
 #include <bsp/mm.h>
 #include <bsp.h>
 #include <libcpu/arm-cp15.h>
-#include <bsp/bootcard.h>
+#include <bsp/arm-cp15-start.h>
+#include <bsp/console.h>
+#include <rtems.h>
+
+static void foo(void)
+{
+  uint32_t ctrl = arm_cp15_get_control();
+
+  printk("MMU is %s enabled!\n", (ctrl & ARM_CP15_CTRL_M) ? "" : "not");
+  printk("C is %s enabled!\n", (ctrl & ARM_CP15_CTRL_C) ? "" : "not");
+  printk("I is %s enabled!\n", (ctrl & ARM_CP15_CTRL_I) ? "" : "not");
+  printk("V is %s enabled!\n", (ctrl & ARM_CP15_CTRL_V) ? "" : "not");
+}
 
 void BSP_START_TEXT_SECTION bsp_start_hook_0( void )
 {
   uint32_t ctrl;
 
-  /*
-   * Do not use bsp_vector_table_begin == 0, since this will get optimized away.
-   */
-  if (bsp_vector_table_end != bsp_vector_table_size) {
-    arm_cp15_set_vector_base_address(bsp_vector_table_begin);
+  arm_cp15_set_vector_base_address(bsp_vector_table_begin);
 
-    ctrl = arm_cp15_get_control();
-    ctrl &= ~ARM_CP15_CTRL_V;
+  ctrl = arm_cp15_get_control();
+  ctrl &= ~ARM_CP15_CTRL_V;
+  arm_cp15_set_control(ctrl);
+
+  ctrl = arm_cp15_get_control();
+
+  if (ctrl & (ARM_CP15_CTRL_I | ARM_CP15_CTRL_C | ARM_CP15_CTRL_M | ARM_CP15_CTRL_A | ARM_CP15_CTRL_Z)) {
+    printk("deactivating MMU...\n");
+    if (ctrl & (ARM_CP15_CTRL_C | ARM_CP15_CTRL_M)) {
+      arm_cp15_data_cache_clean_all_levels();
+      arm_cp15_data_cache_invalidate_all_levels();
+      arm_cp15_flush_prefetch_buffer();
+    }
+    ctrl &= ~(ARM_CP15_CTRL_I | ARM_CP15_CTRL_C | ARM_CP15_CTRL_M | ARM_CP15_CTRL_A | ARM_CP15_CTRL_Z );
     arm_cp15_set_control(ctrl);
+    arm_cp15_instruction_cache_invalidate();
+    arm_cp15_branch_predictor_invalidate_all();
+    arm_cp15_tlb_invalidate();
   }
+
+  foo();
 }
 
 void BSP_START_TEXT_SECTION bsp_start_hook_1( void )
 {
+  uint32_t ctrl;
+
   bsp_start_copy_sections();
+  printk("Before init\n");
+  /* Access is to ttb is too far, > 4kB -> not turning mmu off */
+  bsp_memory_management_initialize();
   bsp_start_clear_bss();
 }
