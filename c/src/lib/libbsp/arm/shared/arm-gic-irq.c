15ad0c455ea3790567f1ff97c2be876fa9874c6f
Andreas Kölbl <andreas.koelbl@st.oth-regensburg.de>
2017-06-06 21:43:36 +0200

    start: Remove boot_cpu

--- c/src/lib/libbsp/arm/shared/arm-gic-irq.c
+++ c/src/lib/libbsp/arm/shared/arm-gic-irq.c
@@ -93,9 +93,6 @@ rtems_status_code bsp_interrupt_facility_initialize(void)
   volatile gic_dist *dist = ARM_GIC_DIST;
   uint32_t id_count = get_id_count(dist);
   uint32_t id;
-  uint8_t boot_cpu;
-
-  boot_cpu = bsp_get_boot_cpu();
 
   arm_cp15_set_exception_handler(
     ARM_EXCEPTION_IRQ,
@@ -107,7 +104,7 @@ rtems_status_code bsp_interrupt_facility_initialize(void)
   }
 
   for (id = 32; id < id_count; ++id) {
-    gic_id_set_targets(dist, id, 1 << boot_cpu);
+    gic_id_set_targets(dist, id, 1 << 0);
   }
 
   cpuif->iccpmr = GIC_CPUIF_ICCPMR_PRIORITY(0xff);
